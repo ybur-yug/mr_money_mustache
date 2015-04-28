c261c3aad90324f4e5e368e08777561373e745d8
Robert H Grayson II <bobbygrayson@gmail.com>
2015-04-28 19:48:40 -0400

    add latest method

--- lib/api.rb
+++ lib/api.rb
@@ -4,12 +4,18 @@ module MrMoneyMustache
   $mmm_url = 'http://www.mrmoneymustache.com'
 
   class Api
+    attr_accessor :browser
+
     def initialize
       @browser = Mechanize.new
     end
 
     def home
       @browser.get($mmm_url)
+    end
+
+    def random
+      @browser.get('http://www.mrmoneymustache.com/?random&post_type=post&post_status=publish')
     end
   end
 end
