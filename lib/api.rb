2f0940769b9a51a350c7fa501628bb2e7ea155b9
Robert H Grayson II <bobbygrayson@gmail.com>
2015-04-29 16:29:51 -0400

    add basic api functions

--- lib/api.rb
+++ lib/api.rb
@@ -1,21 +1,40 @@
 require 'mechanize'
 
 module MrMoneyMustache
-  $mmm_url = 'http://www.mrmoneymustache.com'
-
   class Api
     attr_accessor :browser
 
     def initialize
       @browser = Mechanize.new
+      @urls = {
+        start: 'http://www.mrmoneymustache.com/2013/02/22/getting-rich-from-zero-to-hero-in-one-blog-post/',
+        random: 'http://www.mrmoneymustache.com/?random&post_type=post&post_status=publish',
+        latest: 'http://www.mrmoneymustache.com/blog',
+        home: 'http://www.mrmoneymustache.com'
+      }
     end
 
     def home
-      @browser.get($mmm_url)
+      @browser.get(@urls[:home]).content
+    end
+
+    def start
+      puts get_post @browser.get(@urls[:start])
     end
 
     def random
-      @browser.get('http://www.mrmoneymustache.com/?random&post_type=post&post_status=publish')
+      puts get_post @browser.get(@urls[:random]) 
+    end
+
+    def latest
+      link = @browser.get(@urls[:latest]).links[16].uri.to_s
+      puts get_post @browser.get(link)
+    end
+
+    private
+    def get_post link
+      link.at('.post_box').content
     end
   end
 end
+
